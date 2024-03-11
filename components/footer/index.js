08d3fdd0e7a782a0b73f3f816c39cbd30451aea4
farrasdaffauzan <farrasdaffauzan@gmail.com>
2024-03-11 11:11:40 +0700

    Fisrt Comit

---
+++ components/footer/index.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="mx-auto text-center max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <h1>Mohammad Farras Daffauzan</h1>
+    </div>
+  );
+}
