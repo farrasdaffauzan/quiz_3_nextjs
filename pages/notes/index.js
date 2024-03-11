514f4dfd73e38b563c31f394bbe41880d780c530
farrasdaffauzan <farrasdaffauzan@gmail.com>
2024-03-11 21:55:14 +0700

    Add Modal Delete and try modal add

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,19 +1,46 @@
 import dynamic from "next/dynamic";
-import { Box, Flex, Grid, GridItem, Card, CardBody, CardHeader, CardFooter, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Heading,
+  Text,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useDisclosure } from "@chakra-ui/react";
+import React from "react";
 
 const LayoutComponent = dynamic(() => import("@/layout"));
 
 export default function Notes() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpenAdd, onOpenAdd, onCloseAdd } = useDisclosure();
+
   const router = useRouter();
   const [notes, setNotes] = useState();
 
   const HandleDelete = async (id) => {
     try {
-      const response = await fetch(`api/notes/delete/${id}`);
-      const result = await response.json();
-      if (result?.success) {
+      const response = await (await fetch(`api/notes/delete/${id}`)).json();
+
+      if (response?.success) {
         router.reload();
       }
     } catch (error) {}
@@ -36,9 +63,27 @@ export default function Notes() {
               <Button colorScheme="blue" onClick={() => router.push("/notes/add")}>
                 Add Notes
               </Button>
+              <Button colorScheme="blue" onClick={onOpenAdd}>
+                Add Notes
+              </Button>
+
+              <Modal isOpen={isOpenAdd} onClose={onCloseAdd}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Anda Yakin Akan Menghapus Ini?</ModalHeader>
+                  <ModalCloseButton />
+
+                  <ModalFooter>
+                    <Button onClick={() => router.push("/notes/add")} colorScheme="red">
+                      add
+                    </Button>
+                    <Button onClick={onCloseAdd}>Cancel</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
             </Flex>
             <Flex>
-              <Grid templateColumns="repeat(5, 1fr)" gap={10}>
+              <Grid templateColumns="repeat(2, 1fr)" gap={10}>
                 {notes?.data?.map((item) => (
                   <GridItem>
                     <Card>
@@ -52,9 +97,28 @@ export default function Notes() {
                         <Button onClick={() => router.push(`/notes/edit/${item?.id}`)} flex="1" variant="ghost">
                           Edit
                         </Button>
-                        <Button flex="1" onClick={() => HandleDelete(item?.id)} colorScheme="red">
+                        <Button flex="1" onClick={onOpen} colorScheme="red">
                           Delete
                         </Button>
+                        {/* <Button flex="1" onClick={() => HandleDelete(item?.id)} colorScheme="red">
+                          Delete 1
+                        </Button> */}
+                        {/* <Button onClick={onOpen}>Open Modal</Button> */}
+
+                        <Modal isOpen={isOpen} onClose={onClose}>
+                          <ModalOverlay />
+                          <ModalContent>
+                            <ModalHeader>Anda Yakin Akan Menghapus Ini?</ModalHeader>
+                            <ModalCloseButton />
+
+                            <ModalFooter>
+                              <Button onClick={() => HandleDelete(item?.id)} colorScheme="red">
+                                Delete
+                              </Button>
+                              <Button onClick={onClose}>Cancel</Button>
+                            </ModalFooter>
+                          </ModalContent>
+                        </Modal>
                       </CardFooter>
                     </Card>
                   </GridItem>
